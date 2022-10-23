4c5ea6470f42e8b7287c74beafe34a4825bc656f
shelton gillum <sgillum21@gmail.com>
2022-10-23 12:18:12 -0700

    reset

--- view/client/src/displayFiles/LandingPageHeader.js
+++ view/client/src/displayFiles/LandingPageHeader.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "../styles/landingPage.css"
-import {Link, Links} from "react-router-dom"
+import {Link, Switch, Route} from "react-router-dom"
 export default function LandingPageHeader(props){
     return(
         <div id="landingPageHeaderContainer">
@@ -9,24 +9,24 @@ export default function LandingPageHeader(props){
             <div id="mainButtonContainer">
             
             <div id="buttonContainer1">
-            <Link to="/signupPage"> 
+            
                  <button  className="button-82-pushable" role="button">
                     <span  className="button-82-shadow"></span>
                     <span  className="button-82-edge"></span>
-                    <span  className="button-82-front text"><Link to="/signupPage"></Link>sign up</span>
+                    <span  className="button-82-front text">sign up</span>
                 </button> 
-            </Link>
+            
             
             </div>
             
             <div id="buttonContainer2">
-                <Link to="/login">
-                <button  className="button-82-pushable" role="button">
+             <button  className="button-82-pushable" role="button">
                     <span  className="button-82-shadow"></span>
                     <span  className="button-82-edge"></span>
-                    <span  className="button-82-front text"><Link to="/Login"></Link>login</span>
+                    <span  className="button-82-front text">login</span>
                 </button>
-                </Link>
+                
+                
                 
             
             </div>
--- view/client/src/rootFiles/App.js
+++ view/client/src/rootFiles/App.js
@@ -1,9 +1,11 @@
 import react from "react"
 import HoldingFile from "./HoldingFile"
+
 export default function App(){
     return(
         <div>
             <HoldingFile/>
+            
         </div>
     )
 }
